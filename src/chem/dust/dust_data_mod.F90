396c7fc5c1e8e0c9b0e5cdb4e125e40445268c7a
Barry.Baker <Barry.Baker@noaa.gov>
2019-07-17 18:20:41 +0000

    updating the default values for fengsha at C384

--- src/chem/dust/dust_data_mod.F90
+++ src/chem/dust/dust_data_mod.F90
@@ -38,23 +38,23 @@ module dust_data_mod
   real, parameter :: gocart_gamma  = 1.3
   ! -- FENGSHA
   real, parameter :: fengsha_alpha = 0.3
-  real, parameter :: fengsha_gamma = 1.8
+  real, parameter :: fengsha_gamma = 1.3
   ! -- FENGSHA threshold velocities based on Dale A. Gillette's data
   integer, parameter :: fengsha_maxstypes = 13
   real, dimension(fengsha_maxstypes) :: dust_uthres = &
-    (/ 0.08,   & ! Sand            - 1
-       0.20,   & ! Loamy Sand      - 2
-       0.30,   & ! Sandy Loam      - 3
-       0.30,   & ! Silt Loam       - 4
-       0.35,   & ! Silt            - 5
-       0.60,   & ! Loam            - 6
-       0.30,   & ! Sandy Clay Loam - 7
-       0.35,   & ! Silty Clay Loam - 8
-       0.45,   & ! Clay Loam       - 9
-       0.45,   & ! Sandy Clay      - 10
-       0.45,   & ! Silty Clay      - 11
-       0.60,   & ! Clay            - 12
-       9.999 /)  ! Other           - 13
+    (/ 0.065,   & ! Sand            - 1
+       0.20,    & ! Loamy Sand      - 2
+       0.52,    & ! Sandy Loam      - 3
+       0.50,    & ! Silt Loam       - 4
+       0.50,    & ! Silt            - 5
+       0.60,    & ! Loam            - 6
+       0.73,    & ! Sandy Clay Loam - 7
+       0.73,    & ! Silty Clay Loam - 8
+       0.80,    & ! Clay Loam       - 9
+       0.95,    & ! Sandy Clay      - 10
+       0.95,    & ! Silty Clay      - 11
+       1.00,    & ! Clay            - 12
+       9.999 /)   ! Other           - 13
 
   ! -- values set at initialization
   real :: dust_alpha = 0.
