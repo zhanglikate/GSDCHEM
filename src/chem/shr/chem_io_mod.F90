f7a95998f18ba1e4ca3b1d424fa90b1f6ab9008b
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2019-11-20 04:31:54 +0000

    Extend I/O layer to read/write 4D arrays distributed across multiple tiles.

--- src/chem/shr/chem_io_mod.F90
+++ src/chem/shr/chem_io_mod.F90
@@ -15,6 +15,7 @@ module chem_io_mod
   interface chem_io_read
     module procedure chem_io_read_2DR4
     module procedure chem_io_read_3DR4
+    module procedure chem_io_read_4DR4
   end interface chem_io_read
 
   interface chem_io_write
@@ -22,6 +23,7 @@ module chem_io_mod
     module procedure chem_io_write_2DR8
     module procedure chem_io_write_3DR4
     module procedure chem_io_write_3DR8
+    module procedure chem_io_write_4DR4
   end interface chem_io_write
 
   private
@@ -251,14 +253,19 @@ contains
   end subroutine chem_io_file_read
 
 
-  subroutine chem_io_file_write(datafile, buffer, pos, rc)
+  subroutine chem_io_file_write(datafile, buffer, pos, reccount, recsize, recstride, rc)
     character(len=*),           intent(in)  :: datafile
     real(CHEM_KIND_R4),         intent(in)  :: buffer(:)
     character(len=*), optional, intent(in)  :: pos
+    integer,          optional, intent(in)  :: reccount
+    integer,          optional, intent(in)  :: recsize
+    integer,          optional, intent(in)  :: recstride
     integer,          optional, intent(out) :: rc
 
     ! -- local variables
     integer :: localrc
+    integer :: irec, is, ie
+    integer :: rcount, rsize, rstride
     character(len=6) :: filepos
 
     ! -- begin
@@ -274,13 +281,52 @@ contains
       end select
     end if
 
+    if (present(reccount)) then
+      rcount = reccount
+    else
+      rcount = 1
+    end if
+
+    if (present(recsize)) then
+      rsize = recsize
+    else
+      rsize = size(buffer) / rcount
+    end if
+
+    if (present(recstride)) then
+      rstride = max(rsize, recstride)
+    else
+      rstride = rsize
+    end if
+
+    ! -- adjust record count
+    rcount = size(buffer) / rstride
+
+    if (chem_rc_test((size(buffer) < rcount * rstride), &
+        msg="insufficient buffer size", &
+        file=__FILE__, line=__LINE__, rc=rc)) return
+
+    ! -- open file
     open(unit=ioUnit, file=trim(datafile), form='unformatted', action='write', &
       position=trim(filepos), iostat=localrc)
     if (chem_rc_test((localrc /= 0), msg="Failure opening file: "//trim(datafile), &
         file=__FILE__, line=__LINE__, rc=rc)) return
-    write(unit=ioUnit, iostat=localrc) buffer
-    if (chem_rc_test((localrc /= 0), msg="Failure reading data from file: "//trim(datafile), &
-        file=__FILE__, line=__LINE__, rc=rc)) return
+
+    ! -- write records
+    is = 1
+    ie = rsize
+    do irec = 1, rcount
+      write(unit=ioUnit, iostat=localrc) buffer(is:ie)
+      if (chem_rc_test((localrc /= 0), msg="Failure writing data to file: "//trim(datafile), &
+          file=__FILE__, line=__LINE__, rc=rc)) then
+        close(unit=iounit)
+        return
+      end if
+      is = is + rstride
+      ie = ie + rstride
+    end do
+
+    ! -- close file
     close(unit=ioUnit)
 
   end subroutine chem_io_file_write
@@ -333,7 +379,8 @@ contains
 
       call chem_io_file_name(datafile, filename, tile, pathname=path)
 
-      call chem_io_file_read(datafile, buffer, recrange=recrange, recsize=recsize, recstride=recstride, rc=localrc)
+      call chem_io_file_read(datafile, buffer, recrange=recrange, &
+        recsize=recsize, recstride=recstride, rc=localrc)
       if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
 
       if (chem_io_verbose) &
@@ -409,7 +456,8 @@ contains
 
       call chem_io_file_name(datafile, filename, tile, pathname=path)
 
-      call chem_io_file_read(datafile, buffer, recrange=recrange, recsize=recsize, recstride=recstride, rc=localrc)
+      call chem_io_file_read(datafile, buffer, recrange=recrange, &
+        recsize=recsize, recstride=recstride, rc=localrc)
       if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
 
       if (chem_io_verbose) &
@@ -435,6 +483,118 @@ contains
       file=__FILE__, line=__LINE__, rc=rc)) return
 
   end subroutine chem_io_read_3DR4
+
+
+  subroutine chem_io_read_4DR4(filename, farray, order, path, recsize, recstride, de, rc)
+    character(len=*),           intent(in)  :: filename
+    real(CHEM_KIND_R4),         intent(out) :: farray(:,:,:,:)
+    character(len=*), optional, intent(in)  :: order
+    character(len=*), optional, intent(in)  :: path
+    integer,          optional, intent(in)  :: recsize
+    integer,          optional, intent(in)  :: recstride
+    integer,          optional, intent(in)  :: de
+    integer,          optional, intent(out) :: rc
+
+    ! -- local variables
+    integer :: localrc
+    integer :: tile, tileComm
+    integer :: i, j, k, l, id, jd, lbuf
+    integer :: ids, ide, jds, jde, its, ite, jts, jte, nk, nl
+    logical :: localIOflag
+    character(len=3) :: localOrder
+    character(len=CHEM_MAXSTR) :: datafile
+    real(CHEM_KIND_R4), dimension(:),       allocatable :: buffer
+    real(CHEM_KIND_R4), dimension(:,:,:,:), allocatable, target :: buf4d
+
+    ! -- begin
+    if (present(rc)) rc = CHEM_RC_SUCCESS
+
+    call chem_model_get(de=de, tile=tile, tileComm=tileComm, &
+      localIOflag=localIOflag, rc=localrc)
+    if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
+
+    call chem_model_domain_get(de=de, ids=ids, ide=ide, jds=jds, jde=jde, &
+      its=its, ite=ite, jts=jts, jte=jte, rc=localrc)
+    if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
+
+    localOrder = "ijk"
+    if (present(order)) localOrder = order
+
+    select case (trim(localOrder))
+      case("ikj")
+        ! -- (i,k,j)
+        nk = size(farray, dim=2)
+      case default
+        ! -- default to (i,j,k)
+        nk = size(farray, dim=3)
+    end select
+
+    nl = size(farray, dim=4)
+
+    ! -- check size consistency
+    lbuf = (ide-ids+1) * (jde-jds+1) * nk * nl
+    if (chem_rc_test((size(farray) /= lbuf), &
+      msg="size of input array inconsistent with domain decomposition", &
+      file=__FILE__, line=__LINE__, rc=rc)) return
+
+    lbuf = (ite-its+1) * (jte-jts+1) * nk * nl
+    allocate(buffer(lbuf), stat=localrc)
+    if (chem_rc_test((localrc /= 0), &
+      msg="Cannot allocate read buffer", &
+      file=__FILE__, line=__LINE__, rc=rc)) return
+
+    buffer = 0._CHEM_KIND_R4
+
+    if (localIOflag) then
+
+      call chem_io_file_name(datafile, filename, tile, pathname=path)
+
+      print *,'READ4D: '//trim(filename)//" -> "//trim(datafile)
+
+      call chem_io_file_read(datafile, buffer, recrange=(/ 1, nl /), &
+        recsize=recsize, recstride=recstride, rc=localrc)
+      if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
+
+      if (chem_io_verbose) &
+        write(6,'("chem_io_read: tile=",i2,2x,a," - min/max = "2g16.6)') tile, &
+          trim(datafile), minval(buffer), maxval(buffer)
+    end if
+
+    call chem_comm_bcast(buffer, comm=tileComm, rc=localrc)
+    if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
+
+    allocate(buf4d(its:ite,jts:jte,nk,nl), stat=localrc)
+    if (chem_rc_test((localrc /= 0), &
+      msg="Cannot allocate read buffer", &
+      file=__FILE__, line=__LINE__, rc=rc)) return
+
+    buf4d = reshape(buffer, shape(buf4d))
+
+    select case (trim(localOrder))
+      case("ikj")
+        do l = 1, nl
+          do k = 1, nk
+            j = 0
+            do jd = jds, jde
+              j = j + 1
+              i = 0
+              do id = ids, ide
+                i = i + 1
+                farray(i, k, j, l) = buf4d(id, jd, k, l)
+              end do
+            end do
+          end do
+        end do
+      case default
+        farray = buf4d(ids:ide, jds:jde, :, :)
+    end select
+
+    deallocate(buffer, buf4d, stat=localrc)
+    if (chem_rc_test((localrc /= 0), &
+      msg="Cannot deallocate read buffer", &
+      file=__FILE__, line=__LINE__, rc=rc)) return
+
+  end subroutine chem_io_read_4DR4
 
 
   subroutine chem_io_write_2DR4(filename, farray, path, pos, de, rc)
@@ -776,5 +936,114 @@ contains
         file=__FILE__, line=__LINE__, rc=rc)) return
 
   end subroutine chem_io_write_3DR8
+
+
+  subroutine chem_io_write_4DR4(filename, farray, order, path, pos, recsize, recstride, de, rc)
+    character(len=*),           intent(in)  :: filename
+    real(CHEM_KIND_R4),         intent(in)  :: farray(:,:,:,:)
+    character(len=*), optional, intent(in)  :: order
+    character(len=*), optional, intent(in)  :: path
+    character(len=*), optional, intent(in)  :: pos
+    integer,          optional, intent(in)  :: recsize
+    integer,          optional, intent(in)  :: recstride
+    integer,          optional, intent(in)  :: de
+    integer,          optional, intent(out) :: rc
+
+    ! -- local variables
+    integer :: localrc
+    integer :: tile, tileComm
+    integer :: i, j, k, l, id, jd, lbuf
+    integer :: ids, ide, jds, jde, its, ite, jts, jte, nk, nl
+    logical :: localIOflag
+    character(len=3) :: localOrder
+    character(len=CHEM_MAXSTR) :: datafile
+    real(CHEM_KIND_R4), dimension(:),       allocatable :: recvbuf
+    real(CHEM_KIND_R4), dimension(:,:,:,:), allocatable :: buf4d
+
+    ! -- begin
+    if (present(rc)) rc = CHEM_RC_SUCCESS
+
+    call chem_model_get(de=de, tile=tile, tileComm=tileComm, &
+      localIOflag=localIOflag, rc=localrc)
+    if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
+
+    call chem_model_domain_get(de=de, ids=ids, ide=ide, jds=jds, jde=jde, &
+      its=its, ite=ite, jts=jts, jte=jte, rc=localrc)
+    if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
+
+    localOrder = "ijk"
+    if (present(order)) localOrder = order
+
+    select case (trim(localOrder))
+      case("ikj")
+        ! -- (i,k,j)
+        nk = size(farray, dim=2)
+      case default
+        ! -- default to (i,j,k)
+        nk = size(farray, dim=3)
+    end select
+
+    nl = size(farray, dim=4)
+
+    ! -- check consistency in decomposition
+    if (chem_rc_test((size(farray) /= (ide-ids+1)*(jde-jds+1)*nk*nl), &
+      msg="size of input array inconsistent with domain decomposition", &
+      file=__FILE__, line=__LINE__, rc=rc)) return
+
+    allocate(buf4d(its:ite,jts:jte,nk,nl), stat=localrc)
+    if (chem_rc_test((localrc /= 0), msg="Cannot allocate read buffer", &
+        file=__FILE__, line=__LINE__, rc=rc)) return
+
+    buf4d = 0._CHEM_KIND_R4
+
+    select case (trim(localOrder))
+      case("ikj")
+        do l = 1, nl
+          do k = 1, nk
+            j = 0
+            do jd = jds, jde
+              j = j + 1
+              i = 0
+              do id = ids, ide
+                i = i + 1
+                buf4d(id, jd, k, l) = farray(i, k, j, l)
+              end do
+            end do
+          end do
+        end do
+      case default
+        buf4d(ids:ide, jds:jde, 1:nk, 1:nl) = farray
+    end select
+
+    lbuf = (ite-its+1)*(jte-jts+1)*nk*nl
+
+    allocate(recvbuf(lbuf), stat=localrc)
+    if (chem_rc_test((localrc /= 0), msg="Cannot allocate read buffer", &
+        file=__FILE__, line=__LINE__, rc=rc)) return
+
+    recvbuf = 0._CHEM_KIND_R4
+
+    call chem_comm_reduce(reshape(buf4d, (/lbuf/)), recvbuf, CHEM_COMM_SUM, comm=tileComm, rc=localrc)
+    if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
+
+    if (localIOflag) then
+
+      call chem_io_file_name(datafile, filename, tile, pathname=path)
+      print *,'WRITE4D: '//trim(filename)//" -> "//trim(datafile)
+
+      call chem_io_file_write(datafile, recvbuf, pos=pos, reccount=nl, &
+        recsize=recsize, recstride=recstride, rc=localrc)
+      if (chem_rc_check(localrc, file=__FILE__, line=__LINE__, rc=rc)) return
+
+      if (chem_io_verbose) &
+        write(6,'("chem_io_write: tile=",i2,2x,a," - min/max = "2g16.6)') tile, &
+          trim(datafile), minval(recvbuf), maxval(recvbuf)
+    end if
+
+    deallocate(buf4d, recvbuf, stat=localrc)
+    if (chem_rc_test((localrc /= 0), msg="Cannot deallocate read buffer", &
+        file=__FILE__, line=__LINE__, rc=rc)) return
+
+  end subroutine chem_io_write_4DR4
 
 end module chem_io_mod
